3d8f06d18a82be47c80540a52de6a7fc5f1a1510
Tommy <englerttw@mymail.vcu.edu>
2018-09-07 20:45:10 -0400

    weather data with csv module

---
+++ code/csv_weather/program.py
@@ -0,0 +1,23 @@
+import research
+
+def main():
+
+    research.init()
+    print("Weather research for Seattle, 2014-2015")
+    print()
+    #TODO: Initialize the data
+
+    print("The hottest 5 days:")
+    #TODO: show the days
+
+    print("The coldest 5 days:")
+    #TODO: show the days
+
+    print("The wettest 5 days:")
+    #TODO: show the days
+
+
+
+
+if __name__ == '__main__':
+    main()
